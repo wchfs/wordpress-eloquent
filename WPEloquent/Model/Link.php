74ef98bb10e3cb9ffedb9d6767eb6576b337ad95
wch-mirek <mirek.g@webchefs.pl>
2020-11-10 11:25:44 +0100

    "Link" model typo fix

--- WPEloquent/Model/Link.php
+++ WPEloquent/Model/Link.php
@@ -2,7 +2,7 @@
 
 namespace WPEloquent\Model;
 
-class Links extends \Illuminate\Database\Eloquent\Model {
+class Link extends \Illuminate\Database\Eloquent\Model {
     protected $table      = 'links';
     protected $primaryKey = 'link_id';
     public $timestamps    = false;
